0249dfdf0208e887cdc7402ec5b985c97eee6186
Simon H. <simon.ludvig.hakansson@outlook.com>
2025-03-22 18:59:17 +0100

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ const Button = ({
   <button
     className={cn(
       "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors",
-      "focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-offset-2 focus:ring-offset-gray-950",
+      "focus:outline-none focus:ring-1 focus:ring-orange-500 focus:ring-offset-2 focus:ring-offset-gray-950",
       "disabled:opacity-50 disabled:pointer-events-none",
       className
     )}
@@ -213,7 +213,7 @@ const KQLLibrary = () => {
             <Button
               key={category}
               className={cn(
-                "px-4 py-2 rounded-md font-medium transition-all duration-200",
+                "px-4 py-2 rounded-md font-medium",
                 categoryInfo[category]?.buttonBg,
                 selectedCategory === category
                   ? "bg-gray-700 ring-1 ring-orange-500"
@@ -237,7 +237,7 @@ const KQLLibrary = () => {
               <Button
                 key={subCategory}
                 className={cn(
-                  "px-4 py-2 rounded-md transition-all duration-200 bg-gray-800 hover:bg-gray-700",
+                  "px-4 py-2 rounded-md bg-gray-800 hover:bg-gray-700",
                   selectedSubCategory === subCategory
                     ? "bg-gray-700 ring-1 ring-orange-500"
                     : ""
