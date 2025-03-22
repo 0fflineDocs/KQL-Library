c0126d1d754906ea56f076111b1ee6eb55e8973a
Simon H. <simon.ludvig.hakansson@outlook.com>
2025-03-22 13:03:15 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-function App() {
-  return <h1 className="text-2xl text-orange-500">KQL Library Loaded</h1>;
-}
+import KQLLibrary from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <KQLLibrary />
   </React.StrictMode>
 );
